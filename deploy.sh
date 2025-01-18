61500efae37a858fcf49a6cc18f97e234131ec71
Jesse Nicholas <42322303+JesseNicholas00@users.noreply.github.com>
2025-01-19 01:23:38 +0800

    add deployment script

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+echo "Building project"
+
+env GOOS=linux GOARCH=arm64 go build -o gogo-manager.out
+
+echo "Finished building project"
+
+# Path to your private key
+KEY="../project-sprint-infra/projectsprint.key"
+
+# Files/folders to upload
+FILES="gogo-manager.out .env migrations"
+
+# Destination directory on the remote servers
+DEST="/home/ubuntu/"
+
+# Servers to deploy to
+SERVERS=(
+  "10.0.6.220"
+  "10.0.0.11"
+  "10.0.7.90"
+)
+
+# Loop over servers and upload the files
+for SERVER in "${SERVERS[@]}"; do
+  echo "Uploading files to ${SERVER}..."
+  scp -i "${KEY}" -r ${FILES} "ubuntu@${SERVER}:${DEST}"
+  echo "Done uploading to ${SERVER}."
+  echo
+done
